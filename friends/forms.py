65ae0776337b75a2ea05e5b96b11b32c6baf4cea
George Hickman <george@ghickman.co.uk>
2012-05-10 17:23:22 +0100

    Update messages use for 1.4

--- friends/forms.py
+++ friends/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.conf import settings
-
+from django.contrib import messages
 from django.contrib.auth.models import User
 
 from friends.models import *
@@ -17,7 +17,7 @@ else:
 
 
 class UserForm(forms.Form):
-    
+
     def __init__(self, user=None, *args, **kwargs):
         self.user = user
         super(UserForm, self).__init__(*args, **kwargs)
@@ -25,37 +25,45 @@ class UserForm(forms.Form):
 
 if EmailAddress:
     class JoinRequestForm(forms.Form):
-        
+
         email = forms.EmailField(label="Email", required=True, widget=forms.TextInput(attrs={'size':'30'}))
         message = forms.CharField(label="Message", required=False, widget=forms.Textarea(attrs = {'cols': '30', 'rows': '5'}))
-        
+
+        def __init__(self, *args, **kwargs):
+            self.request = kwargs.pop('reqeust')
+            super(JoinRequestForm, self).__init__(*args, **kwargs)
+
         def clean_email(self):
             # @@@ this assumes email-confirmation is being used
             self.existing_users = EmailAddress.objects.get_users_for(self.cleaned_data["email"])
             if self.existing_users:
                 raise forms.ValidationError(u"Someone with that email address is already here.")
             return self.cleaned_data["email"]
-        
+
         def save(self, user):
             join_request = JoinInvitation.objects.send_invitation(user, self.cleaned_data["email"], self.cleaned_data["message"])
-            user.message_set.create(message="Invitation to join sent to %s" % join_request.contact.email)
+            messages.info(self.request, 'Invitation to join sent to {0}'.format(join_request.contact.email))
             return join_request
 
 
 class InviteFriendForm(UserForm):
-    
+
     to_user = forms.CharField(widget=forms.HiddenInput)
     message = forms.CharField(label="Message", required=False, widget=forms.Textarea(attrs = {'cols': '20', 'rows': '5'}))
-    
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request')
+        super(InviteFriendForm, self).__init__(*args, **kwargs)
+
     def clean_to_user(self):
         to_username = self.cleaned_data["to_user"]
         try:
             User.objects.get(username=to_username)
         except User.DoesNotExist:
             raise forms.ValidationError(u"Unknown user.")
-            
+
         return self.cleaned_data["to_user"]
-    
+
     def clean(self):
         to_user = User.objects.get(username=self.cleaned_data["to_user"])
         previous_invitations_to = FriendshipInvitation.objects.invitations(to_user=to_user, from_user=self.user)
@@ -66,7 +74,7 @@ class InviteFriendForm(UserForm):
         if previous_invitations_from.count() > 0:
             raise forms.ValidationError(u"%s has already requested friendship with you" % to_user.username)
         return self.cleaned_data
-    
+
     def save(self):
         to_user = User.objects.get(username=self.cleaned_data["to_user"])
         message = self.cleaned_data["message"]
@@ -75,5 +83,5 @@ class InviteFriendForm(UserForm):
         if notification:
             notification.send([to_user], "friends_invite", {"invitation": invitation})
             notification.send([self.user], "friends_invite_sent", {"invitation": invitation})
-        self.user.message_set.create(message="Friendship requested with %s" % to_user.username) # @@@ make link like notification
+        messages.info(request, 'Friendship requested with {0}'.format(to_user.username)) # @@@ make link like notification
         return invitation
